d3acc83c15ff0e14344a0e2dd135d3def2990d76
eduramza <eduardo.rmb@outlook.com>
2020-08-15 09:57:16 -0300

    layout criado

---
+++ app/src/main/java/com/eduramza/googlenewsapp/HomeActivity.kt
@@ -0,0 +1,18 @@
+package com.eduramza.googlenewsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.eduramza.googlenewsapp.ui.main.HomeFragment
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, HomeFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
